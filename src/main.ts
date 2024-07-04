3b908e6306962969d4ce18021f2db34a649544f4
iAmStringGYJ <95227958+iAmStringGYJ@users.noreply.github.com>
2024-07-04 09:44:57 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,23 @@ import { isEmail, isPhone } from './utils/validate'
 ;(async () => {
   const spinner = ora('正在刷步中...').start()
   try {
-    const account = `2365069711@qq.com`
-    const password = `123456789aB`
+    // 从环境变量中获取账号和密码
+    const account = process.env.ACCOUNT
+    const password = process.env.PASSWORD
     const step = randomFullClose(18650, 25906) // 根据指定范围生成随机步数
-    if (!isPhone(account) && !isEmail(account)) throw new Error(`请检查账号是否为手机号或邮箱`)
+
+    if (!account || !password) {
+      throw new Error('账号或密码未设置')
+    }
+
+    if (!isPhone(account) && !isEmail(account)) {
+      throw new Error('请检查账号是否为手机号或邮箱')
+    }
+
     const zeppLife = new ZeepLife(account, password, step)
     const data = await zeppLife.main()
     spinner.succeed(`${data.date} ${data.account} 刷步完成 ${data.step}`)
-  } catch (error: any) {
+  } catch (error) {
     spinner.fail(`${dayjs().format('YYYY-MM-DD HH:mm:ss')} [Error]: ${error.message || '未知异常'}`)
   }
 })()
